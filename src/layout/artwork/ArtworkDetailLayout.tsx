1008fabdd7255075a9b51fc7d0e009b47384a90a
whilini <whi0306@naver.com>
2022-12-17 11:35:27 +0900

    feat: share artwork url

--- src/layout/artwork/ArtworkDetailLayout.tsx
+++ src/layout/artwork/ArtworkDetailLayout.tsx
@@ -39,7 +39,7 @@ const ArtworkDetailLayout = ({ artistDetail, isLoading }: ArtworkDetailProps) =>
       });
     } else if (navigator.clipboard) {
       navigator.clipboard
-        .writeText(`https://m.wwweb.kr/artist-detail/${artistDetail?.id}`)
+        .writeText(`https://m.wwweb.kr/artwork-detail/${artistDetail?.id}`)
         .then(() => alert('링크가 클립보드에 복사되었습니다.'));
     } else {
       alert('공유하기가 지원되지 않는 환경 입니다.');
